d8c3ab6332eee1ec24c616e04297b74977afa8a5
Lugon LQ <48004924+lugondev@users.noreply.github.com>
2022-08-31 10:04:04 +0700

    wip: multicall  - raw data

--- bin/dart_web3_multicall.dart
+++ bin/dart_web3_multicall.dart
@@ -1,4 +1,5 @@
 import 'package:dart_web3_multicall/dart_web3_multicall.dart' as dart_web3_multicall;
+import 'package:dart_web3_multicall/fake_data.dart' as fake_data;
 import 'package:dart_web3_multicall/multicall.dart' as multicall;
 import 'package:http/http.dart'; //You can also import the browser version
 import 'package:web3dart/web3dart.dart';
@@ -9,12 +10,16 @@ var httpClient = Client();
 var ethClient = Web3Client(apiUrl, httpClient);
 var multicallContract = EthereumAddress.fromHex("0x7aa35985B617416F502afC69b25aB9dBF7FDd0a1");
 
-var token1 = EthereumAddress.fromHex("0x4F32336a6032BA3763D533853224f743b3C8eE88");
-var token2 = EthereumAddress.fromHex("0x8085c02665f2BC0975Bd69C747D1918c3154e5c0");
+var token = EthereumAddress.fromHex("0x8085c02665f2BC0975Bd69C747D1918c3154e5c0");
+var user = EthereumAddress.fromHex("0x7dedcd37ad8d65c4c01b522e30060836ffbf81bb");
 
 void main(List<String> arguments) async {
   print('Hello world: ${dart_web3_multicall.calculate()}!');
 
-  List<dynamic> results = await multicall.getTokenDetails(ethClient, multicallContract, token1);
-  print(results);
+  List<dynamic> tokenDetails = await multicall.getTokenDetails(ethClient, multicallContract, token);
+  print(tokenDetails);
+
+  List<fake_data.Token> tokensList = fake_data.fakeTokensList();
+  List<dynamic> balanceOfs = await multicall.getBalanceMultiTokens(ethClient, multicallContract, tokensList, user);
+  print(balanceOfs);
 }
--- lib/multicall.dart
+++ lib/multicall.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:dart_web3_multicall/fake_data.dart';
 import 'package:web3dart/web3dart.dart';
 
 DeployedContract getDeployedMulticall(EthereumAddress multicallContract) {
@@ -10,8 +11,13 @@ DeployedContract getDeployedMulticall(EthereumAddress multicallContract) {
 
 DeployedContract getDeployedToken(EthereumAddress tokenContract) {
   String multicallAbiJson = File('lib/token.abi.json').readAsStringSync();
-  ContractAbi contractABI = ContractAbi.fromJson(multicallAbiJson, "TokenERc20");
+  ContractAbi contractABI = ContractAbi.fromJson(multicallAbiJson, "TokenERC20");
   return DeployedContract(contractABI, tokenContract);
+}
+
+ContractFunction getBalanceOfFunction() {
+  String multicallAbiJson = File('lib/token.abi.json').readAsStringSync();
+  return ContractAbi.fromJson(multicallAbiJson, "TokenERC20").functions.firstWhere((element) => element.name == "balanceOf");
 }
 
 getTokenDetails(Web3Client ethClient, EthereumAddress multicallContract, EthereumAddress tokenContract) async {
@@ -29,6 +35,20 @@ getTokenDetails(Web3Client ethClient, EthereumAddress multicallContract, Ethereu
       [tokenContract, decimalsFunction.encodeCall([])]
     ]
   ]);
+
+  return resultAggregate;
+}
+
+getBalanceMultiTokens(Web3Client ethClient, EthereumAddress multicallContract, List<Token> tokensList, EthereumAddress userAddress) async {
+  DeployedContract multicallDeployedContract = getDeployedMulticall(multicallContract);
+
+  ContractFunction balanceOfFunction = getBalanceOfFunction();
+  List<dynamic> callsData = List<dynamic>.from(tokensList.map((token) => List<dynamic>.from([
+        token.address,
+        balanceOfFunction.encodeCall([userAddress])
+      ])));
+
+  List<dynamic> resultAggregate = await ethClient.call(contract: multicallDeployedContract, function: multicallDeployedContract.function("aggregate"), params: [callsData]);
 
   return resultAggregate;
 }
